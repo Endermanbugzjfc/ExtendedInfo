463071c0e8cf508dc554e7725aff49b21a621be9
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-31 03:13:15 +0800

    Added user interface profiles data class

---
+++ ExtendedInfo/src/UserInterfaceProfiles.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ExtendedInfo;
+
+final class UserInterfaceProfiles
+{
+    public string $classicUI = "Classic UI";
+    public string $pocketUI = "Pocket UI";
+}
