3acbf0948e9fc22de4596ca11274ec6bf9a40305
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-31 04:03:30 +0800

    Added index to item trait

---
+++ ExtendedInfo/src/Plugin/IndexToItemTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ExtendedInfo\Plugin;
+
+use RuntimeException;
+
+trait IndexToItemTrait
+{
+    /**
+     * Maps index to the corresponding item from a data class that its properties are arranged in ascending order.
+     *
+     * Log exception and the first item if the index has no corresponding item in this data class.
+     *
+     * Then wait a few centuries until some server owners are willing to report the exception. And I figured out what item is missing.
+     */
+    public function fromIndex(
+        int $index
+    ) : string {
+        $map = (array)$this;
+        $return = $map[$index] ?? null;
+
+        if ($return === null) {
+            $return = $map[0];
+            $class = $this::class;
+            $err = new RuntimeException("Index $index has no corresponding item in data class $class");
+            Loader::getInstance()->getLogger()->logException($err);
+        }
+        return $return;
+    }
+}
