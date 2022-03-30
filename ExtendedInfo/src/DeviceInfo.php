95f301e2348201714e1383b85669f9946fbed073
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-31 02:21:50 +0800

    Added device info

---
+++ ExtendedInfo/src/DeviceInfo.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ExtendedInfo;
+
+use SOFe\InfoAPI\Info;
+use SOFe\InfoAPI\StringInfo;
+
+final class DeviceInfo extends Info
+{
+    public function __construct(
+        private StringInfo $operatingSystem,
+        private StringInfo $deviceModel,
+        private StringInfo $userInterfaceProfile,
+        private StringInfo $guiScale,
+        private StringInfo $controls, // Current Input Mode is indeed a confusing name.
+    ) {
+    }
+
+    public function toString() : string
+    {
+    }
+}
