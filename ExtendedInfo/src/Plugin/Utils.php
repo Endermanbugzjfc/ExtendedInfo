1a539f51c2913d0c9ddccb85e6e9becb4eb04740
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-31 03:36:49 +0800

    Added the unknown item util function

--- ExtendedInfo/src/Plugin/Utils.php
+++ ExtendedInfo/src/Plugin/Utils.php
@@ -2,11 +2,22 @@
 
 declare(strict_types=1);
 
-namespace Endermanbugzjfc\ExtendedInfo;
+namespace Endermanbugzjfc\ExtendedInfo\Plugin;
 
 final class Utils
 {
     private function __construct()
     {
+    }
+
+    /**
+     * For data class that has an "unkown" item. Log exception if the index is unkown for this plugin. Return the unkown item at the end.
+     *
+     * Then wait a few centuries until some server owners are willing to report the exception. And I figured out what item is missing in the plugin.
+     */
+    public static function unknownItem(
+        string $unknown,
+        int $index
+    ) : string {
     }
 }
