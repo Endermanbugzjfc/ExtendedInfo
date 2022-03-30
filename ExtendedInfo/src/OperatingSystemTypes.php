99da9633b05aab0f48e25f970883b39fee198b23
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-31 03:10:05 +0800

    Added operating system types data class

---
+++ ExtendedInfo/src/OperatingSystemTypes.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ExtendedInfo;
+
+final class OperatingSystemTypes
+{
+    public string $unknown = "Unknown";
+    public string $android = "Android";
+    public string $iOS = "iOS";
+    public string $macOS = "macOS";
+    public string $fireOS = "FireOS";
+    public string $gearVR = "GearVR";
+    public string $holoLens = "HoloLens";
+    public string $windows10 = "Windows 10";
+    public string $windows = "Windows";
+    public string $dedicated = "Dedicated";
+    public string $orbis = "Orbis";
+    public string $playstation4 = "Playstation 4";
+    public string $nintentoSwitch = "Nintento Switch";
+    public string $xboxOne = "Xbox One";
+}
