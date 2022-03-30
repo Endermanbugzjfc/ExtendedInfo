9a29e46e883d49964fafc55e509b086178afcd19
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-31 03:17:45 +0800

    Added controls data class

---
+++ ExtendedInfo/src/Controls.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ExtendedInfo;
+
+final class Controls
+{
+    public string $unknown = "Unknown";
+    public string $mouse = "Mouse";
+    public string $touch = "Touch";
+    public string $controller = "Controller";
+}
