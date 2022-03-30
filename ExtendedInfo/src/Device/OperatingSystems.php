20fdacb65d79a9326ae72e975d9f51499b14b45c
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-31 03:44:18 +0800

    Added from index functions

--- ExtendedInfo/src/Device/OperatingSystems.php
+++ ExtendedInfo/src/Device/OperatingSystems.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\ExtendedInfo\Device;
 
+use Endermanbugzjfc\ExtendedInfo\Plugin\Utils;
+
 final class OperatingSystems
 {
     public string $unknown = "Unknown";
@@ -20,4 +22,25 @@ final class OperatingSystems
     public string $playstation4 = "Playstation 4";
     public string $nintentoSwitch = "Nintento Switch";
     public string $xboxOne = "Xbox One";
+
+    public function fromIndex(
+        int $index
+    ) : string {
+        return match ($index) {
+            1 => $this->android,
+            2 => $this->iOS,
+            3 => $this->macOS,
+            4 => $this->fireOS,
+            5 => $this->gearVR,
+            6 => $this->holoLens,
+            7 => $this->windows10,
+            8 => $this->windows,
+            9 => $this->dedicated,
+            10 => $this->orbis,
+            11 => $this->playstation4,
+            12 => $this->nintentoSwitch,
+            13 => $this->xboxOne,
+            default => Utils::unknownItem($this->unknown, $index)
+        };
+    }
 }
--- ExtendedInfo/src/Device/UserInterfaceProfiles.php
+++ ExtendedInfo/src/Device/UserInterfaceProfiles.php
@@ -4,8 +4,20 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\ExtendedInfo\Device;
 
+use RuntimeException;
+
 final class UserInterfaceProfiles
 {
     public string $classicUI = "Classic UI";
     public string $pocketUI = "Pocket UI";
+
+    public function fromIndex(
+        int $index
+    ) : string {
+        return match ($index) {
+            0 => $this->classicUI,
+            1 => $this->pocketUI,
+            default => throw new RuntimeException("unknown user interface profile index $index")
+        };
+    }
 }
