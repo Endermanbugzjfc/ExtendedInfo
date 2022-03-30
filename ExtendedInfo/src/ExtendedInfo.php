cb614d7ead108b50d6d2ac1a13a18f598c61084f
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-31 02:08:02 +0800

    Added plugin main class

---
+++ ExtendedInfo/src/ExtendedInfo.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ExtendedInfo;
+
+use pocketmine\plugin\PluginBase;
+
+final class ExtendedInfo extends PluginBase
+{
+    protected function onLoad() : void
+    {
+        self::$instance = $this;
+    }
+
+    protected function onEnable() : void
+    {
+    }
+
+    private static self $instance;
+
+    public static function getInstance() : self
+    {
+        return self::$instance;
+    }
+}
